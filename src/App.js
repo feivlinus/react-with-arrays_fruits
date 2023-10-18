0253e1a0c568dd0ba4af8dc08d83fe5c33c4bf45
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-18 11:35:06 +0200

    Completed challenge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Fragment } from "react";
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 0, name: "🍌 banana", color: "yellow" },
+    { id: 1, name: "🍎 apple", color: "red" },
+    { id: 2, name: "🍍 pineapple", color: "yellow" },
+    { id: 3, name: "🥝 kiwi", color: "green" },
+    { id: 4, name: "🫐 blueberry", color: "purple" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {/* <Card name="🍌 banana" /> */}
+      {fruits.map(({ id, name, color }) => (
+        <Fragment key={id}>
+          <Card name={name} color={color} />
+        </Fragment>
+      ))}
     </div>
   );
 }
